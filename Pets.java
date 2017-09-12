aa83c74afa3cb1e8d0a5b42fb29fa0fb12a7511a
Dianne11 <diannedeiparine11@gmail.com>
2017-09-12 17:03:38 +0800

    Update Pets.java

--- Pets.java
+++ Pets.java
@@ -1,3 +1,5 @@
+import tester.Tester;
+
 // to represent a pet owner
 class Person {
     String name;
@@ -9,7 +11,29 @@ class Person {
         this.pet = pet;
         this.age = age;
     }
+    
+  /*TEMPLATE
+   Fields
+	  ...this.name - string
+	  ...this.pet - IPet
+	  ...this.age - int
+  
+   Methods
+   	...isOlder - boolean
+   */
+
+
+boolean isOlder(Person other){
+	return this.age > other.age;
 }
+    
+// Person -> boolean
+// Returns true if this Person is older than the given Person
+/* boolean isOlder(Person other) {
+	 	return false;
+ } */
+}
+
 // to represent a pet
 interface IPet { }
 
@@ -37,4 +61,31 @@ class Dog implements IPet {
         this.kind = kind;
         this.male = male;
     }
+}
+
+
+class ExamplesPet {
+	
+	ExamplesPet() {}
+	
+	IPet snow = new Cat("Snow", "Himalayan", true);
+	IPet ginger = new Cat("Ginger", "Ocicat", false);
+	IPet darky = new Dog("Darky", "Doberman", true);
+	IPet bella = new Dog("Bella", "Shih Tzu", false);
+	
+	Person shayne = new Person("Shayne", snow, 19);
+	Person dianne = new Person("Dianne", ginger, 19);
+	Person danny = new Person("Danny", darky, 32);
+	Person franky = new Person("Franky", bella, 27);
+	
+	boolean testPet(Tester t){
+		return 
+			t.checkExpect(this.shayne.isOlder(dianne), false) &&
+		    t.checkExpect(this.franky.isOlder(danny),false) &&
+		    t.checkExpect(this.danny.isOlder(shayne),true) &&
+		    t.checkExpect(this.franky.isOlder(dianne),true) &&
+		    t.checkExpect(this.dianne.isOlder(danny), false) &&
+		    t.checkExpect(this.franky.isOlder(shayne), true);
+		
+	}
 }
