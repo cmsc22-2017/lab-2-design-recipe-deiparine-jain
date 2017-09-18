593958f9b746df5d0c7648f37024bca3cfa76d42
Dianne11 <diannedeiparine11@gmail.com>
2017-09-18 12:34:07 +0800

    Update Pets.java

--- Pets.java
+++ Pets.java
@@ -1,5 +1,6 @@
 import tester.Tester;
 
+
 // to represent a pet owner
 class Person {
     String name;
@@ -22,20 +23,67 @@ class Person {
    	...isOlder - boolean
    */
 
-
-boolean isOlder(Person other){
-	return this.age > other.age;
-}
-    
+	boolean isOlder(Person other){
+		return this.age > other.age;
+	}
 // Person -> boolean
 // Returns true if this Person is older than the given Person
 /* boolean isOlder(Person other) {
 	 	return false;
  } */
+	
+/*TEMPLATE
+  Fields
+	  ...this.name - string
+	  ...this.pet - IPet
+	  ...this.age - int
+ 
+  Methods
+  	...perish - Person
+*/
+
+Person perish() {
+		return new Person(this.name, new NoPet(), this.age);
+}
+	
+//for method perish
+// -> Person
+//Produces this Person's name whose pet has perished
+/* String perish() {
+	return null;
+}*/
+
 }
 
+class NoPet implements IPet{
+	/*Person p;
+	boolean hasPerished;
+	
+	NoPet(Person p, boolean hasPerished) {
+		this.p = p;	
+		this.hasPerished = hasPerished;
+	}*/
+	
+	NoPet() {}
+
+	@Override
+	public boolean sameNamePet(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/*public boolean sameNamePet(String name) {
+  	return this.name.equals(name);
+  }*/
+	
+}
+
+
+
 // to represent a pet
-interface IPet { }
+interface IPet {
+	boolean sameNamePet(String name); 
+}
 
 // to represent a pet cat
 class Cat implements IPet {
@@ -48,6 +96,28 @@ class Cat implements IPet {
         this.kind = kind;
         this.longhaired = longhaired;
     }
+    
+    /*TEMPLATE
+		Fields
+	 ...this.name - string
+	 ...this.kind - String
+	 ...this.longhaired - boolean
+
+		Methods
+			...sameNamePet - boolean
+*/
+    
+    public boolean sameNamePet(String name) {
+    	return this.name.equals(name);
+    }
+
+
+    // for method sameNamePet
+    //String -> boolean
+    //Determines whether this name matches the given name
+    /*boolean sameNamePet () {
+    	return false;
+    }*/
 }
 
 // to represent a pet dog
@@ -61,9 +131,32 @@ class Dog implements IPet {
         this.kind = kind;
         this.male = male;
     }
+    
+    /*TEMPLATE
+		Fields
+	 ...this.name - string
+	 ...this.kind - String
+	 ...this.male - boolean
+
+		Methods
+			...sameNamePet - boolean
+*/
+    
+    public boolean sameNamePet(String name) {
+    	return this.name == name;
+    }
+
+
+    // for method sameNamePet
+    //String -> boolean
+    //Determines whether this name matches the given name
+    /*boolean sameNamePet () {
+    	return false;
+    }*/
 }
 
 
+//Examples
 class ExamplesPet {
 	
 	ExamplesPet() {}
@@ -73,6 +166,8 @@ class ExamplesPet {
 	IPet darky = new Dog("Darky", "Doberman", true);
 	IPet bella = new Dog("Bella", "Shih Tzu", false);
 	
+	NoPet noPet = new NoPet();
+	
 	Person shayne = new Person("Shayne", snow, 19);
 	Person dianne = new Person("Dianne", ginger, 19);
 	Person danny = new Person("Danny", darky, 32);
@@ -80,11 +175,40 @@ class ExamplesPet {
 	
 	boolean testPet(Tester t){
 		return 
-		    t.checkExpect(this.shayne.isOlder(dianne), false) &&
+				t.checkExpect(this.shayne.isOlder(dianne), false) &&
 		    t.checkExpect(this.franky.isOlder(danny),false) &&
 		    t.checkExpect(this.danny.isOlder(shayne),true) &&
 		    t.checkExpect(this.franky.isOlder(dianne),true) &&
 		    t.checkExpect(this.dianne.isOlder(danny), false) &&
 		    t.checkExpect(this.franky.isOlder(shayne), true);
+		
+	}
+	
+	boolean testSameNamePet(Tester t) {
+		return
+				t.checkExpect(this.snow.sameNamePet("Olivia"), false) &&
+				t.checkExpect(this.bella.sameNamePet("Edward"), false) &&
+				t.checkExpect(this.ginger.sameNamePet("Ginger"), true) &&
+				t.checkExpect(this.darky.sameNamePet("Darky"), true);
+		
+	}
+	
+	boolean testPerish(Tester t) {
+		Person a = this.danny.perish();
+		Person b = this.dianne.perish();
+		Person c = this.franky.perish();
+		
+		return
+				t.checkExpect(a.name, "Danny") &&
+				t.checkExpect(a.pet, noPet) &&
+				t.checkExpect(a.age, 32) &&
+				t.checkExpect(b.name, "Dianne") &&
+				t.checkExpect(b.pet, noPet) &&
+				t.checkExpect(b.age, 19) &&
+				t.checkExpect(c.name, "Franky") &&
+				t.checkExpect(c.pet, noPet) &&
+				t.checkExpect(c.age, 27);
+		
 	}
 }
+
